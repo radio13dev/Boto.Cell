cb0a208054ff96168da0c7f43ad5eb49329f5a00
Ben Wheatley <radio13dev@gmail.com>
2025-12-12 10:10:41 +1000

    Unity version 6.4.05a

--- Packages/com.arongranberg.aline/AlineURPRenderPassFeature.cs
+++ Packages/com.arongranberg.aline/AlineURPRenderPassFeature.cs
@@ -11,19 +11,6 @@ namespace Drawing {
 	public class AlineURPRenderPassFeature : ScriptableRendererFeature {
 		/// <summary>Custom Universal Render Pipeline Render Pass for ALINE</summary>
 		public class AlineURPRenderPass : ScriptableRenderPass {
-			/// <summary>This method is called before executing the render pass</summary>
-#if MODULE_RENDER_PIPELINES_UNIVERSAL_17_0_0_OR_NEWER
-			[System.Obsolete]
-#endif
-			public override void Configure (CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor) {
-			}
-
-#if MODULE_RENDER_PIPELINES_UNIVERSAL_17_0_0_OR_NEWER
-			[System.Obsolete]
-#endif
-			public override void Execute (ScriptableRenderContext context, ref RenderingData renderingData) {
-				DrawingManager.instance.ExecuteCustomRenderPass(context, renderingData.cameraData.camera);
-			}
 
 			public AlineURPRenderPass() : base() {
 				profilingSampler = new ProfilingSampler("ALINE");
